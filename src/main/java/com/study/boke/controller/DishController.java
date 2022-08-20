546b6e459dfe1dae476730da0e4ce17b9e03cfdc
llz <1091351262@qq.com>
2022-08-20 21:50:13 +0800

    1

--- src/main/java/com/study/boke/controller/DishController.java
+++ src/main/java/com/study/boke/controller/DishController.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.study.boke.common.R;
 import com.study.boke.dto.DishDto;
+import com.study.boke.entity.Category;
 import com.study.boke.entity.Dish;
 import com.study.boke.entity.DishFlavor;
 import com.study.boke.service.CategoryService;
@@ -13,10 +14,12 @@ import com.study.boke.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @RestController
@@ -29,10 +32,15 @@ public class DishController {
     CategoryService categoryService;
     @Autowired
     DishFlavorService dishFlavorService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @PostMapping
     public void save(@RequestBody DishDto dishDto){
+
         dishService.DishAndFlavor(dishDto);
+        String key = "dish_" + dishDto.getCategoryId() + "_1";
+        redisTemplate.delete(key);
     }
 
     @GetMapping("/page")
@@ -68,28 +76,45 @@ public class DishController {
     @PutMapping
     public R<String> update(@RequestBody DishDto dishDto){
         dishService.updateDishWithFlavor(dishDto);
+
+        String key = "dish_" + dishDto.getCategoryId() + "_1";
+        redisTemplate.delete(key);
         return R.success("修改成功！");
     }
 
     @GetMapping("/list")
     public R<List<DishDto>> list(@RequestBody Dish dish){
+        List<DishDto> dishDtoList = null;
+
+        String key = "dish_"+dish.getCategoryId() +"_" + dish.getStatus();
+        dishDtoList = (List<DishDto>) redisTemplate.opsForValue().get(key);
+        if (dishDtoList!=null){
+            return R.success(dishDtoList);
+        }
+
         LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(dish.getCategoryId()!=null,Dish::getCategoryId,dish.getCategoryId());
         queryWrapper.orderByAsc(Dish::getSort).orderByDesc(Dish::getUpdateTime);
         queryWrapper.eq(Dish::getStatus,1);
         List<Dish> dishList = dishService.list(queryWrapper);
-        List<DishDto> dishDtoList = new ArrayList<>();
-        List<DishDto> collect = dishList.stream().map((item) -> {
+
+        dishDtoList = dishList.stream().map((item) -> {
             DishDto dishDto = new DishDto();
             BeanUtils.copyProperties(item, dishDto);
+            Long categoryId = item.getCategoryId();
+            Category category = categoryService.getById(categoryId);
+            if (category!=null){
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
             LambdaQueryWrapper<DishFlavor> queryWrapper1 = new LambdaQueryWrapper<>();
             queryWrapper1.eq(item.getId() != null, DishFlavor::getDishId, item.getId());
             List<DishFlavor> flavorList = dishFlavorService.list(queryWrapper1);
             dishDto.setFlavors(flavorList);
-            dishDtoList.add(dishDto);
             return dishDto;
         }).collect(Collectors.toList());
 
+        redisTemplate.opsForValue().set(key,dishDtoList,10, TimeUnit.MINUTES);
         return R.success(dishDtoList);
     }
 }
--- src/main/java/com/study/boke/controller/UserController.java
+++ src/main/java/com/study/boke/controller/UserController.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -27,14 +29,17 @@ public class UserController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
     @PostMapping("/sendMsg")
     public R<String> sendMsg(@RequestBody User user,HttpSession session){
         String phone = user.getPhone();
         if (!StringUtils.isEmpty(phone)){
             String code = ValidateCodeUtils.generateValidateCode4String(4);
-            code = "1234";
+            System.out.println(code);
             //SMSUtils.sendMessage("[安大吴奇隆]","",phone,code);
-            session.setAttribute(phone,code);
+            //session.setAttribute(phone,code);
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
             return R.success("短信发送成功");
         }
         return R.success("短信发送失败");
@@ -44,7 +49,8 @@ public class UserController {
     public R<User> sendMsg(@RequestBody Map map, HttpSession session){
          String phone = map.get("phone").toString();
          String code = map.get("code").toString();
-        Object codeInSession = session.getAttribute(phone);
+        //Object codeInSession = session.getAttribute(phone);
+        Object codeInSession = redisTemplate.opsForValue().get(phone);
         if (codeInSession!=null && code.equals(codeInSession)){
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
             queryWrapper.eq(User::getPhone,phone);
@@ -55,6 +61,7 @@ public class UserController {
                 user.setStatus(1);
                 userService.save(user);
             }
+            redisTemplate.delete(phone);
             return R.success(user);
         }
         return R.error("登录失败");
